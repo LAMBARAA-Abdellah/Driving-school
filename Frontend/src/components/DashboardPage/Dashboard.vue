9e617983c9e5e0e630bc9070392d926dd1391183
lambaraa abdellah <abdellahlambaraa@gmail.com>
2022-06-02 09:48:38 +0100

    add

--- Frontend/src/components/DashboardPage/Dashboard.vue
+++ Frontend/src/components/DashboardPage/Dashboard.vue
@@ -80,18 +80,90 @@
 </div>
   </section>
 </div>
+
+<div class="statistique">
+
+  <Bar
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :dataset-id-key="datasetIdKey"
+    :plugins="plugins"
+    :css-classes="cssClasses"
+    :styles="styles"
+    :width="width"
+    :height="height"
+  />
+  </div>
 </template>
 
-<script >
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 export default {
     name: 'Dashboard',
-    props: {
-        msg: String
+ components: { Bar },
+  props: {
+    chartId: {
+      type: String,
+      default: 'bar-chart'
+    },
+    datasetIdKey: {
+      type: String,
+      default: 'label'
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 400
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object,
+      default: () => {
+        return {
+          
+        }
+      }
+    },
+    plugins: {
+      type: Object,
+      default: () => {}
     }
+  },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
+        datasets: [ { data: [40, 20, 12,24, 20, 12,30, 20, 12,20, 20, 12,] ,
+        backgroundColor:["#F8CE03"],
+        label:"Chaifre d'affaire"
+        
+        
+        
+        
+        } ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
 }
 
 </script>
 
 <style lang="scss" scoped>
-
+.statistique{ 
+  width: 50%;
+  height: 400px;
+}
 </style>
--- Frontend/src/components/auth/chart.vue
+++ Frontend/src/components/auth/chart.vue
@@ -1,75 +1,58 @@
 <template>
-  <Bar
-    :chart-options="chartOptions"
-    :chart-data="chartData"
-    :chart-id="chartId"
-    :dataset-id-key="datasetIdKey"
-    :plugins="plugins"
-    :css-classes="cssClasses"
-    :styles="styles"
-    :width="width"
-    :height="height"
-  />
+
+   <div id="app">
+    world
+    <pie-chart :data="chartData" :options="chartOptions"></pie-chart>
+
+  </div>
 </template>
-
 <script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
+import { Doughnut, mixins } from "vue-chartjs";
 export default {
-  name: 'BarChart',
-  components: { Bar },
-  props: {
-    chartId: {
-      type: String,
-      default: 'bar-chart'
-    },
-    datasetIdKey: {
-      type: String,
-      default: 'label'
-    },
-    width: {
-      type: Number,
-      default: 400
-    },
-    height: {
-      type: Number,
-      default: 400
-    },
-    cssClasses: {
-      default: '',
-      type: String
-    },
-    styles: {
-      type: Object,
-      default: () => {
-        return {
-          
-        }
-      }
-    },
-    plugins: {
-      type: Object,
-      default: () => {}
-    }
+  name: "App",
+  components: {
+    PieChart,
+  },
+  extends: Doughnut,
+  props: ["data", "options"],
+  mounted() {
+    // this.chartData is created in the mixin.
+    // If you want to pass options please create a local options object
+    this.renderChart(this.data, {
+      borderWidth: "10px",
+      hoverBackgroundColor: "red",
+      hoverBorderWidth: "10px"
+    });
   },
   data() {
     return {
-      chartData: {
-        labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
-        datasets: [ { data: [40, 20, 12,24, 20, 12,30, 20, 12,20, 20, 12,] ,
-        backgroundColor:["#F8CE03"],
-        
-        
-        
-        } ]
-      },
       chartOptions: {
-        responsive: true
-      }
-    }
-  }
-}
+        hoverBorderWidth: 10,
+      },
+      chartData: {
+        hoverBackgroundColor: "red",
+        hoverBorderWidth: 6,
+        labels: ["Green", "Red", "Blue"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: ["#41B883", "#E46651", "#00D8FF"],
+            data: [1, 10, 5],
+          },
+        ],
+      },
+    };
+  },
+};
 </script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
